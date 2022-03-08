0b57d9f4dcb8327ca3a21d959eb76e815edea8e7
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-03-08 12:30:59 +0100

    Add simple public folder + html file res

--- week4/W4D2/live/npm-intro/index.js
+++ week4/W4D2/live/npm-intro/index.js
@@ -1,10 +1,14 @@
-// first have to npm i express in your directory for this to work!
+// first have to npm i express in your director for this to work!
 const express = require("express");
 const app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (request, response) => {
   console.log("Hello world, we got a get request!!!!!");
-  response.send("Hello world from our own server");
+  //   response.send("Hello world from our own server");
+
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(3000, () => {
