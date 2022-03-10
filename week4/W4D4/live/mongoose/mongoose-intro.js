46b2c1fb3922f4872a646d1c3350bfba7bcefb1c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-03-10 11:26:23 +0100

    Setters, validators and such.

--- week4/W4D4/live/mongoose/mongoose-intro.js
+++ week4/W4D4/live/mongoose/mongoose-intro.js
@@ -1,5 +1,33 @@
 const mongoose = require("mongoose");
 
+// not related to mongoose - more about how module.exports and require work
+const ourModule = require("./our-own-module");
+console.log(ourModule);
+
+function padPokeTypesToLength(pokeTypes, length) {
+  for (let i = pokeTypes.length; i < length; i++) {
+    pokeTypes.push("");
+  }
+  return pokeTypes;
+}
+
+function completePokeTypes(pokeTypes) {
+  if (pokeTypes.length === 2) {
+    return pokeTypes;
+  }
+  if (pokeTypes.length < 2) {
+    const paddedPokeTypes = padPokeTypesToLength(pokeTypes, 2);
+    console.log(paddedPokeTypes);
+    return paddedPokeTypes;
+  }
+  return pokeTypes;
+}
+
+const generationsEnum = [];
+for (let i = 0; i < 7; i++) {
+  generationsEnum.push(i + 1);
+}
+
 const pokemonSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,6 +37,38 @@ const pokemonSchema = new mongoose.Schema({
   hp: {
     type: Number,
     required: true,
+    min: 0,
+    max: 1000,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  generation: {
+    type: Number,
+    enum: generationsEnum,
+    default: 1,
+  },
+  moves: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: (movesArray) => {
+        return movesArray.length === 4;
+      },
+      message: "Pokemons should have 4 moves",
+    },
+    default: ["", "", "", ""],
+  },
+  pokeTypes: {
+    type: [String],
+    set: completePokeTypes,
+    validate: {
+      validator: (movesArray) => {
+        return movesArray.length === 2;
+      },
+      message: "Pokemons should have exactly 2 types",
+    },
   },
 });
 
@@ -27,22 +87,33 @@ async function main() {
   const pikachu = new Pokemon({
     name: "Pikachu",
     hp: 30,
+    moves: ["Thunderbolt", "", "", ""],
+    pokeTypes: ["Electro"],
   });
 
   await pikachu.save();
 
-  const pikachu2 = new Pokemon({
-    name: "Pikachu",
-    hp: 50,
-  });
+  //   const pikachu2 = new Pokemon({
+  //     name: "Pikachu",
+  //     hp: 50,
+  //   });
 
-  await pikachu2.save();
+  //   await pikachu2.save();
 
   const charizard = new Pokemon({
     name: "Charizard",
     hp: 150,
+    pokeTypes: ["Fire", "Dragon"],
   });
   await charizard.save();
+
+  const bulbasaur = new Pokemon({
+    name: "Bulbasaur",
+    hp: 0,
+    pokeTypes: ["Grass", "Flower"],
+    generation: 1,
+  });
+  await bulbasaur.save();
 
   await mongoose.connection.close();
 }
