048b5ff26a60f10a8a6c4e8f1ec9de962c3881b0
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-03-29 12:30:39 +0200

    Add Counter example

--- week7/W7D2/live/state-intro/src/App.js
+++ week7/W7D2/live/state-intro/src/App.js
@@ -1,15 +1,52 @@
 import "./App.css";
-import { useState } from "react";
 import { NameToggle } from "./components/NameToggle";
 import { HarryPotterCharacters } from "./components/HarryPotterCharacters";
-import { ConditionalRendering } from "./components/ConditionalRendering";
+import { useState } from "react";
+// import { ConditionalRendering } from "./components/ConditionalRendering";
+
+function Counter({ defaultValue = 0, increment = 1 }) {
+  const [number, setNumber] = useState(defaultValue);
+  const incrementNumber = () => {
+    // number++
+    // number = number + 1;
+    // number += 1;
+    // console.log(number++);
+    console.log("number before update", number);
+    const update = number + increment; // ++number // number++
+    console.log("number:", number);
+    console.log("update:", update);
+    setNumber(update);
+  };
+
+  const decrementNumber = () => {
+    setNumber(number - increment);
+  };
+
+  const reset = () => {
+    setNumber(defaultValue);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {number}</h1>
+      <button onClick={incrementNumber}>+{increment}</button>
+      <button onClick={decrementNumber}>-{increment}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-      <ConditionalRendering />
+      <Counter increment={5} />
+      <Counter defaultValue={100} />
+      <Counter defaultValue={1000} increment={100} />
+
+      {/* <ConditionalRendering /> */}
       <NameToggle />
       <HarryPotterCharacters />
+
       {/* <HarryPotterCharacters /> */}
       {/* <HarryPotterCharacters /> */}
 
