06f2061052d905e0d8fa1c455dbda4863e7148d7
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-03-30 14:16:43 +0200

    Remove filter

--- week7/W7D3/planned/harry-potter-forms/src/App.js
+++ week7/W7D3/planned/harry-potter-forms/src/App.js
@@ -1,102 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+import { Character } from "./components/Character";
 import { characters as defaultCharacters } from "./data/charactersWithImages";
-
-function Character({ character, changeCharacter }) {
-  const [editMode, setEditMode] = useState(false);
-  const [characterForm, setCharacterForm] = useState(character);
-  const toggleEditMode = () => {
-    setEditMode(!editMode);
-    changeCharacter(characterForm);
-  };
-
-  const handleFormUpdateForField = (fieldName) => (event) => {
-    console.log("Hello");
-    const newState = { ...characterForm, [fieldName]: event.target.value };
-    console.log(newState);
-    setCharacterForm(newState);
-  };
-
-  return (
-    <div className="card harryPotterCard">
-      <div className="row g-0 cardRow">
-        <div className="col-4">
-          <img
-            src={character.image}
-            className="characterImage rounded-start"
-            alt={character.name}
-          />
-        </div>
-        <div className="col-8">
-          <div className="card-body">
-            {editMode ? (
-              <>
-                <h5 className="card-title">
-                  Name:{" "}
-                  <input
-                    value={characterForm.name}
-                    onChange={handleFormUpdateForField("name")}
-                  />
-                </h5>
-                <p className="card-text">
-                  House:{" "}
-                  <input
-                    value={characterForm.house}
-                    onChange={handleFormUpdateForField("house")}
-                  />
-                </p>
-                <p className="card-text">
-                  Patronus:{" "}
-                  <input
-                    value={characterForm.patronus}
-                    onChange={handleFormUpdateForField("patronus")}
-                  />
-                </p>
-                <p className="card-text">
-                  Actor:{" "}
-                  <input
-                    value={characterForm.actor}
-                    onChange={handleFormUpdateForField("actor")}
-                  />
-                </p>
-                <p className="card-text">
-                  Gender:{" "}
-                  <input
-                    value={characterForm.gender}
-                    onChange={handleFormUpdateForField("gender")}
-                  />
-                </p>
-                <p className="card-text">
-                  Ancestry:{" "}
-                  <input
-                    value={characterForm.ancestry}
-                    onChange={handleFormUpdateForField("ancestry")}
-                  />
-                </p>
-              </>
-            ) : (
-              <>
-                <h5 className="card-title"> Name: {character.name}</h5>
-                <p className="card-text">House: {character.house}</p>
-                <p className="card-text">Patronus: {character.patronus}</p>
-                <p className="card-text">Actor: {character.actor}</p>
-                <p className="card-text">Gender: {character.gender}</p>
-                <p className="card-text">Ancestry: {character.ancestry}</p>
-              </>
-            )}
-            <button onClick={toggleEditMode}>
-              {editMode ? "Save" : "Edit"}
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 function App() {
   const [characters, setCharacters] = useState(defaultCharacters);
-  const [filter, setFilter] = useState("");
   const changeCharacter = (index) => (update) => {
     const updatedCharacters = characters.map((character, current) => {
       if (index === current) {
@@ -105,10 +13,6 @@ function App() {
       return character;
     });
     setCharacters(updatedCharacters);
-  };
-
-  const handleFilterInput = (event) => {
-    setFilter(event.target.value);
   };
 
   const toCharacterComponent = (character, index) => {
@@ -124,13 +28,7 @@ function App() {
     <div className="App">
       <input value={filter} onChange={handleFilterInput}></input>
       <div className="harry-potter-characters">
-        {filter === ""
-          ? characters.map(toCharacterComponent)
-          : characters
-              .filter((character) =>
-                character.name.toLowerCase().includes(filter.toLowerCase())
-              )
-              .map(toCharacterComponent)}
+        {characters.map(toCharacterComponent)}
       </div>
     </div>
   );
