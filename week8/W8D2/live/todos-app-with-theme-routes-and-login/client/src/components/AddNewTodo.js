eace5ab732f61b9c5a35339a247c6a78c36ffa8f
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-04-05 08:59:55 +0200

    Add starter code from yesterday

---
+++ week8/W8D2/live/todos-app-with-theme-routes-and-login/client/src/components/AddNewTodo.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { API_BASE_URL } from "../consts";
+
+export function AddNewTodo({ setAllTodos }) {
+  const [newTodo, setNewTodo] = useState({ name: "", done: false });
+  const handleFormInput = (event) => {
+    setNewTodo({ ...newTodo, [event.target.name]: event.target.value });
+  };
+
+  const handleAddNewTodo = async () => {
+    if (newTodo.name.length === 0) {
+      alert("Please enter a name for the todo!");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/todos`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTodo),
+      });
+      const serverResponse = await response.json();
+      console.log(serverResponse);
+      setAllTodos((oldTodos) => {
+        return [...oldTodos, serverResponse.todo];
+      });
+    } catch (error) {
+      console.error("Error in updating the todo list on the server!", error);
+    }
+  };
+
+  return (
+    <div>
+      <input name="name" value={newTodo.name} onChange={handleFormInput} />
+      <button onClick={handleAddNewTodo}>Add New Todo!</button>
+    </div>
+  );
+}
